de487f5e16f0f93993502a2aa2ace62608f99865
josedev21 <josedev257@gmail.com>
2024-08-05 14:04:03 -0300

    ajustando herança

--- veiculos.js
+++ veiculos.js
@@ -1,18 +1,26 @@
 /**
     **Class Veiculo retorna apenas o nome e marca do modelo;
 
-    
  */
 
     function Veiculo(nome, modelo) {
         this.nome = nome;
         this.modelo = modelo;
-        // this.infoVeiculo = function() {
-        //     console.log(this.nome + ' Veiculo registrado')
-        // }
+        this.infoVeiculo = function() {
+            console.log(`Seu carro é um ${nome} ${modelo}`)
+        }
     }
 
-    const Carro = new Veiculo("Golf", "volkswagen")
-    console.log(Carro)
-    console.log(Carro.nome)
-    // Carro.infoVeiculo();
+    function Carro(motor, ano, preço) {
+        this.motor = motor;
+        this.ano = ano;
+        this.preço = preço
+
+        Veiculo.call(this, nome, modelo)
+    }
+
+    const Carro1 = new Carro('Volksvagem', 'Golf', '1.6 Turbo', 2022, 100.000)
+
+    console.log(Carro1)
+
+
